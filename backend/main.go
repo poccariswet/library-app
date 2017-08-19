c0282f72496e06babec08f12007dcefcf1cccb6b
soeyusuke <s1240231@gmail.com>
2017-08-20 01:20:14 +0900

    [add] middleware

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gocraft/dbr"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 var (
@@ -15,11 +16,12 @@ var (
 func main() {
 	e := echo.New()
 
+	e.Use(middleware.CORS())
 	// Routing
 	e.GET("/book/pullbookinfo/:id", Pull_Book_Info)
 	e.GET("/book/pullbooksinfo/all", Pull_Books_Info)
 	e.PUT("/book/updatebookinfo", Update_Book_Info)
-	e.POST("/books/postinfo", Post_Book_Info)
+	e.POST("/book/postbookinfo", Post_Book_Info)
 	e.DELETE("/book/deletebookinfo/:id", Delete_Book_Info)
 
 	e.Logger.Fatal(e.Start(":9090"))
