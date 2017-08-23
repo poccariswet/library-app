41abb7b6b56319920df2d966488fd6dc6e256f49
soeyusuke <s1240231@gmail.com>
2017-08-23 19:42:06 +0900

    [delete] fix

--- src/main.js
+++ src/main.js
@@ -3,12 +3,8 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
-import VueSuperagent from 'vue-superagent'
 
 Vue.use(ElementUI)
-Vue.use(VueSuperagent, {
-  baseUrl: 'localhost:9090'
-})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
