8294788bad8f040213e7226c3f9fd6ecbe42a56f
soeyusuke <s1240231@gmail.com>
2017-08-23 19:40:42 +0900

    [add] router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/components/home'
+import AllBooks from '@/components/allbooks'
+import GetInfo from '@/components/getinfo'
+import PostInfo from '@/components/postinfo'
 
 Vue.use(Router)
-
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: '/home',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/book/pullbooksinfo/all',
+      name: 'allbooks',
+      component: AllBooks
+    },
+    {
+      path: '/book/pullbookinfo/:id',
+      name: 'getinfo',
+      component: GetInfo
+    },
+    {
+      path: '/book/postbookinfo/',
+      name: 'postinfo',
+      component: PostInfo
+    },
+    {
+      path: '*',
+      redirect: '/home'
     }
   ]
 })
