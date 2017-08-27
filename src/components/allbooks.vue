8e58a38e6dc4df6d5105a6aacd104e725e6136d9
soeyusuke <s1240231@gmail.com>
2017-08-27 21:46:49 +0900

    [add] pic & text style

--- src/components/allbooks.vue
+++ src/components/allbooks.vue
@@ -2,18 +2,19 @@
   <div id="allbooks">
     <h1>{{ Info }}</h1>
       <div class="bar">
-        <el-input type="text" v-model="searchString" placeholder="Enter your search terms" icon="search" />
+        <el-input type="text" v-model="searchString" placeholder="Enter your search book" icon="search" />
       </div>
-      <ul>
-        <li v-for="book in filteredBooks">
+      <div class="block">
+        <ul v-for="book in filteredBooks">
           <img v-bind:src="book.Pic">
           <p>{{book.Title}}</p>
-        </li>
       </ul>
+      <!-- <#pagenation>-->
+    </div>
     <el-button type="primary" @click="push">ホームへ</el-button>
+    <el-button type="primary" @click="push_PostInfo">本の追加へ</el-button>
   </div>
 </template>
-
 
 <script>
 import Utils from '../request'
@@ -23,6 +24,8 @@ export default {
     return {
       Info: 'AllBooksInfo',
       searchString: '',
+      // page: 0,
+      // ItemSize: 100,
       Books: [
         {
           Id: '',
@@ -42,6 +45,9 @@ export default {
   methods: {
     push () {
       this.$router.push({ name: 'home' })
+    },
+    push_PostInfo () {
+      this.$router.push({ name: 'postinfo' })
     },
     set () {
     },
@@ -77,15 +83,47 @@ export default {
 </script>
 
 <style scoped>
-#allbooks h1{
+#allbooks{
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  margin-top: 60px;
+  margin-top: 10px;
+}
+/*topの色*/
+h1 {
   color: #6495ED;
-},
-ul {
-  
+}
+
+ul{
+  text-align: left;
+}
+
+ul img {
+  width:50px;
+  height:70px;
+  float:left;
+  border:none;
+}
+
+ul p {
+  margin-left: 50px;
+  margin: 30px;
+  font-weight: bold;
+  padding-top: 30px;
+  padding: 20px;
+  color:#6e7a7f;
+}
+
+p:hover{
+  color: #FF6347;
+  cursor: pointer;
 }
 </style>
+
+<!--
+<el-pagination
+  layout="prev, pager, next"
+  :total="this.ItemSize">
+</el-pagination>
+ -->
